d3a31f5bab6376a5533344e79e0924e3fd888dbd
saket105 <saketbsc123@gmail.com>
2025-09-06 02:00:35 +0530

    jenkins file added

--- src/main/java/com/saket/firstspring/FirstSpringApplication.java
+++ src/main/java/com/saket/firstspring/FirstSpringApplication.java
@@ -1,12 +1,15 @@
 package com.saket.firstspring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class FirstSpringApplication {
 
 	public static void main(String[] args) {
+		log.info("Spring application is starting....");
 		SpringApplication.run(FirstSpringApplication.class, args);
 	}
 
--- src/main/java/com/saket/firstspring/controller/HelloController.java
+++ src/main/java/com/saket/firstspring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.saket.firstspring.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,21 +10,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
+        log.info("hello get url is getting called: {}","hello");
         return "<h1>Hello Spring...</h1>";
     }
 
     @PostMapping("/hello")
     public String helloPost(@RequestBody String name){
+        log.info("hello post method url is getting called: {}",name);
         return "Hello "+name+"!";
     }
 
     @GetMapping("/")
     public Map<String, Object> getValues(){
+        log.info("root url is getting called.....");
         Map<String,Object> data = new HashMap<>();
         data.put("message","Java api is working fine");
         data.put("languages", Arrays.asList("Java","JavaScript","Python","GoLang"));
